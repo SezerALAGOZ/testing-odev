89b4059ea68938759ef1f03b49e46aaa75f825e7
Sezer Alagoz <sezeralagoz8@gmail.com>
2021-11-06 22:12:58 +0300

    header test added

---
+++ src/EmojiResults.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+
+import EmojiResultRow from "./EmojiResultRow";
+import "./EmojiResults.css";
+
+export default class EmojiResults extends PureComponent {
+  static propTypes = {
+    emojiData: PropTypes.array
+  };
+
+  render() {
+    return (
+      <div className="component-emoji-results">
+        {this.props.emojiData.map(emojiData => (
+          <EmojiResultRow
+            key={emojiData.title}
+            symbol={emojiData.symbol}
+            title={emojiData.title}
+          />
+        ))}
+      </div>
+    );
+  }
+}
---
+++ src/Header.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { default as userEvent } from "@testing-library/user-event";
+import Header from './Header';
+
+
+describe('Tests for Header', () => {
+    beforeEach(() => {
+        render(<Header />);
+    });
+
+    test("App should render the header", () => {
+        const headerText = screen.getByText('Emoji Search');
+        expect(headerText).toBeInTheDocument();
+    });
+
+    test("Images should be in the document", () => {
+        const imageList = screen.getAllByRole("image");
+        expect(imageList.length).toEqual(2);
+    });
+})
+
+
+
+
